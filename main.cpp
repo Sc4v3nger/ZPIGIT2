557f560da12dfa6c6cb8d90ca7093270871a7271
Sc4v3nger <wtopolski10@gmail.com>
2020-03-19 22:11:16 +0100

    commit Slack

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout << "odejmowanie" << endl;
-    int licz1, licz2;
-    cout << "podaj 1 liczbę: ";
-    cin >> licz1;
-    cout << "podaj 2 liczbę: ";
-    cin >> licz2;
-    int odejmowanie;
-    odejmowanie = licz1 - licz2;
-    cout << "Wynik odejmowania wynosi: " << odejmowanie;
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
